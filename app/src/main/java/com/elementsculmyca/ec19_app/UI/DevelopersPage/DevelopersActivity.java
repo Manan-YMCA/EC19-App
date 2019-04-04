7fbe530193035c08f4859de3f89bebaa6cc35fe2
Jayati <jayatibhayana@gmail.com>
2019-04-04 18:08:00 +0530

    dev

---
+++ app/src/main/java/com/elementsculmyca/ec19_app/UI/DevelopersPage/DevelopersActivity.java
@@ -0,0 +1,15 @@
+package com.elementsculmyca.ec19_app.UI.DevelopersPage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.elementsculmyca.ec19_app.R;
+
+public class DevelopersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developers);
+    }
+}
